d6852adaf3bae6e0b53962dc051fd89b630b0ba3
gdsgsa <1793065760@qq.com>
2021-08-11 21:46:59 +0800

    第四次修改

--- demo_test/src/com/whg/test/TestMainTwo.java
+++ demo_test/src/com/whg/test/TestMainTwo.java
@@ -3,7 +3,5 @@ package com.whg.test;
 public class TestMainTwo {
     public static void main(String[] args){
         System.out.println("helloWorld");
-        System.out.println("我是你爸爸");
-        System.out.println("肯定成功");
     }
 }
